ec6a5170d32d0da1c3997920e1dd5b5733ef85f9
Guga Zimmermann <gugazimmermann@gmail.com>
2024-02-10 17:26:07 -0300

    contact and cover page

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "landingPageContactEN": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "landingPageContactEN": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
